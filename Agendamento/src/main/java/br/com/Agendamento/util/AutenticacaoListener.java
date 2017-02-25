2eca34e6eb8573d5918b303bb907f84fe0679b64
sidney2017 <sidney2017@1a4eaf07-57c7-4848-92fc-b55b2508f1fb>
2017-02-25 18:40:12 +0000

    complementando a parte de autenticação

--- Agendamento/src/main/java/br/com/Agendamento/util/AutenticacaoListener.java
+++ Agendamento/src/main/java/br/com/Agendamento/util/AutenticacaoListener.java
@@ -4,23 +4,45 @@ import javax.faces.event.PhaseEvent;
 import javax.faces.event.PhaseId;
 import javax.faces.event.PhaseListener;
 
+import org.omnifaces.util.Faces;
+
+import br.com.Agendamento.bean.AutenticacaoBean;
+import br.com.Agendamento.domain.Usuario;
+
 @SuppressWarnings("serial")
 public class AutenticacaoListener implements PhaseListener {
 
 	@Override
 	public void afterPhase(PhaseEvent event) {
-		System.out.println("Depois da fase.: "+event.getPhaseId());
+
+		String paginaAtual = Faces.getViewId();
+		System.out.println("página atual.: " + paginaAtual);
+
+		boolean ePaginaDeAutenticacao = paginaAtual.contains("autenticacao.xhtml");
+
+		if (!ePaginaDeAutenticacao) {
+			AutenticacaoBean autenticacaoBean = Faces.getSessionAttribute("autenticacaoBean");
+
+			if (autenticacaoBean == null) {
+				Faces.navigate("/Pages/autenticacao.xhtml");
+				return;
+			}
+
+			Usuario usuario = autenticacaoBean.getUsuariologado();
+			if (usuario == null) {
+				Faces.navigate("/Pages/autenticacao.xhtml");
+				return;
+			}
+		}
 	}
 
 	@Override
 	public void beforePhase(PhaseEvent event) {
-		System.out.println("Antes da fase.: "+event.getPhaseId());
-
 	}
 
 	@Override
 	public PhaseId getPhaseId() {
-		return PhaseId.ANY_PHASE;
+		return PhaseId.RESTORE_VIEW;
 	}
 
 }
